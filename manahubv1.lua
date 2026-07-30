a9013d53ed83e7f9c7df45a2e4531214e43b152d
kingmanahub <trimnpro@gmail.com>
2026-07-31 00:15:05 +0700

    Modify auto panic to kick on moderator join
    Change auto panic behavior to kick players when a moderator joins instead of unloading.

--- manahubv1.lua
+++ manahubv1.lua
@@ -24377,17 +24377,21 @@ end
             end
 
             utility:Connection(plrs.PlayerAdded, function(player)
-                if is_moderator_check(player) then
-                    if Toggles and Toggles.auto_panic and Toggles.auto_panic.Value and
-                       Options and Options.auto_panic_options and Options.auto_panic_options.Value and
-                       Options.auto_panic_options.Value["Unload on mod join"] then
-                        utility:plain_webhook(string.format("**AUTO PANIC** Unloading because a moderator joined - %s (%s)", player.Name, player.UserId))
-                        task.wait(0.05)
-                        utility:Unload()
-                    end
-                end
-            end)
+    if is_moderator_check(player) then
+        if Toggles and Toggles.auto_panic and Toggles.auto_panic.Value and
+           Options and Options.auto_panic_options and Options.auto_panic_options.Value and
+           Options.auto_panic_options.Value["Kick on mod join"] then -- Đổi tên điều kiện cho khớp với Menu UI
+           
+            -- Sửa lại tin nhắn gửi về webhook cho đúng ngữ cảnh
+            utility:plain_webhook(string.format("**AUTO PANIC** Kicking because a moderator joined - %s (%s)", player.Name, player.UserId))
+            
+            task.wait(0.05)
+            
+            -- Thay thế hàm Unload() bằng lệnh Kick
+            plr:Kick("Auto Panic: Văng game do có Moderator tham gia!")
         end
+    end
+end)
 
         do
             local detected_illusionists = {}
